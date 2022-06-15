b54908de2521bf7422079a0c2c374821e98dcbc2
kwaktaemin <kwak9898@github.com>
2022-06-15 12:26:47 +0900

    logger

--- src/main/kotlin/com/example/myapp/MyappApplication.kt
+++ src/main/kotlin/com/example/myapp/MyappApplication.kt
@@ -1,9 +1,12 @@
 package com.example.myapp
 
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import javax.annotation.PostConstruct
+
+private val logger = KotlinLogging.logger {}
 
 @SpringBootApplication
 class MyappApplication {
@@ -19,4 +22,11 @@ class MyappApplication {
 
 fun main(args: Array<String>) {
 	runApplication<MyappApplication>(*args)
+
+	// Logging
+	logger.trace { "I'm trace! Hello, world!" }
+	logger.debug { "I'm debug! Hello, world!" }
+	logger.info { "I'm info! Hello, world!" }
+	logger.warn { "I'm warn! Hello, world!" }
+	logger.error { "I'm error! Hello, world!" }
 }
