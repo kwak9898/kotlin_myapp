db6fa22b99f7ecde97f726899853821d6427c2d6
kwaktaemin <kwak9898@github.com>
2022-06-15 10:29:09 +0900

    yml dev delete & main application update

--- src/main/kotlin/com/example/myapp/MyappApplication.kt
+++ src/main/kotlin/com/example/myapp/MyappApplication.kt
@@ -1,10 +1,21 @@
 package com.example.myapp
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class MyappApplication
+class MyappApplication {
+
+	@Value("\${my-app.welcome-message}")
+	val myAppWelcomeMessage: String = ""
+
+	@PostConstruct
+	fun printMyAppWelcomeMessage() {
+		println(myAppWelcomeMessage)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<MyappApplication>(*args)
